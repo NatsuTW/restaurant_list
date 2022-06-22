3d03775e8a70b52a8d5a9ff1c0e1073da7c81f5a
Natsu <natsu.tw0919@gmail.com>
2022-06-23 07:03:49 +0900

    install express-handlebars and set template engine

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 //require packages
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
+
+// set template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 // routes setting
 app.get('/', (req, res) => {
