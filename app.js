e0f5f5146f90e17e97b17a4e86586a43bf303689
Natsu <natsu.tw0919@gmail.com>
2022-06-23 06:38:56 +0900

    Initialize project: build the local server and set the root path

---
+++ app.js
@@ -0,0 +1,14 @@
+//require packages
+const express = require('express')
+const app = express()
+const port = 3000
+
+// routes setting
+app.get('/', (req, res) => {
+  res.send('This is my first project')
+})
+
+// listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost: ${port}`)
+})
