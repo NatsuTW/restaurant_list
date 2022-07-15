cc33c4ffc9a729cf17194baac71201e7dad12800
Natsu <natsu.tw0919@gmail.com>
2022-07-15 17:56:37 +0900

    set the root path to render show page

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
 
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
+})
 // listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on localhost: ${port}`)
