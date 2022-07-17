f9119bc3c68695941c8b345a6bad809e285b484d
Natsu <natsu.tw0919@gmail.com>
2022-07-18 00:10:51 +0900

    Revise root path to render no_result.handlebars when there is no result after user enters the keyword

--- app.js
+++ app.js
@@ -26,9 +26,13 @@ app.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim()
   const restaurants = restaurantList.results.filter(restaurant => {
     return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword.toLowerCase())
-    
   })
-  res.render('index', { restaurants: restaurants, keyword: keyword })
+
+  if (restaurants.length > 0) {
+    res.render('index', { restaurants: restaurants, keyword: keyword })
+  } else {
+    res.render('no_result', { keyword: keyword })
+  }
 })
 
 // listen on the Express server
